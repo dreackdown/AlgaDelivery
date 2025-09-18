dfd656b5367216c54ef83d402d22dc10927aeb54
Hugo Faria <hugofaria157@live.com>
2025-09-18 19:23:06 -0300

    feat: adicionando domain services para delivery

---
+++ Microservices/Delivery-Tracking/src/main/java/dev/hugofaria/algadelivery/delivery/tracking/api/controller/DeliveryController.java
@@ -0,0 +1,31 @@
+package dev.hugofaria.algadelivery.delivery.tracking.api.controller;
+
+import dev.hugofaria.algadelivery.delivery.tracking.api.model.DeliveryInput;
+import dev.hugofaria.algadelivery.delivery.tracking.domain.model.Delivery;
+import dev.hugofaria.algadelivery.delivery.tracking.domain.service.DeliveryPreparationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/deliveries")
+@RequiredArgsConstructor
+public class DeliveryController {
+
+    private final DeliveryPreparationService deliveryPreparationService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Delivery draft(@RequestBody @Valid DeliveryInput input) {
+        return deliveryPreparationService.draft(input);
+    }
+
+    @PutMapping("/{deliveryId}")
+    public Delivery edit(@PathVariable UUID deliveryId,
+                         @RequestBody @Valid DeliveryInput input) {
+        return deliveryPreparationService.edit(deliveryId, input);
+    }
+}
