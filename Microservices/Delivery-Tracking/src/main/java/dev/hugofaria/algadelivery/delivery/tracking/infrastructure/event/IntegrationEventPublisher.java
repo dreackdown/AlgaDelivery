0c7a90ac72ea336c40b595f8b921e89401d8f600
Hugo Faria <hugofaria157@live.com>
2025-09-26 10:52:13 -0300

    feat: implementando integracao dos ms com eureka

---
+++ Microservices/Delivery-Tracking/src/main/java/dev/hugofaria/algadelivery/delivery/tracking/infrastructure/event/IntegrationEventPublisher.java
@@ -0,0 +1,6 @@
+package dev.hugofaria.algadelivery.delivery.tracking.infrastructure.event;
+
+public interface IntegrationEventPublisher {
+
+    void publish(Object event, String key, String topic);
+}
