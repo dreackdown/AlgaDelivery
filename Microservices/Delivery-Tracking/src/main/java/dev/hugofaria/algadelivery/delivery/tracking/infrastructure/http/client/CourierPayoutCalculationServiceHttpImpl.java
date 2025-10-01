41486b72910006538db179e69504cd99ceddc03c
Hugo Faria <hugofaria157@live.com>
2025-09-30 21:18:06 -0300

    feat: Configurando Timeout Pattern no Spring RestClient

--- Microservices/Delivery-Tracking/src/main/java/dev/hugofaria/algadelivery/delivery/tracking/infrastructure/http/client/CourierPayoutCalculationServiceHttpImpl.java
+++ Microservices/Delivery-Tracking/src/main/java/dev/hugofaria/algadelivery/delivery/tracking/infrastructure/http/client/CourierPayoutCalculationServiceHttpImpl.java
@@ -3,18 +3,28 @@ package dev.hugofaria.algadelivery.delivery.tracking.infrastructure.http.client;
 import dev.hugofaria.algadelivery.delivery.tracking.domain.service.CourierPayoutCalculationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
-public class CourierPayoutCalculationServiceHttpImpl implements CourierPayoutCalculationService {
+public class CourierPayoutCalculationServiceHttpImpl
+        implements CourierPayoutCalculationService {
 
     private final CourierAPIClient courierAPIClient;
 
     @Override
     public BigDecimal calculatePayout(Double distanceInKm) {
-        var courierPayoutResultModel = courierAPIClient.payoutCalculation(new CourierPayoutCalculationInput(distanceInKm));
-        return courierPayoutResultModel.getPayoutFee();
+        try {
+            var courierPayoutResultModel = courierAPIClient.payoutCalculation(
+                    new CourierPayoutCalculationInput(distanceInKm));
+            return courierPayoutResultModel.getPayoutFee();
+        } catch (ResourceAccessException e) {
+            throw new GatewayTimeoutException(e);
+        } catch (HttpServerErrorException | IllegalArgumentException e) {
+            throw new BadGatewayException(e);
+        }
     }
 }
